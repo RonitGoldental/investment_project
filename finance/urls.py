eb838a512a126737b5d3060dcd12bb96eefbcaab
RonitGoldental <ronit.goldental@gmail.com>
2019-11-26 11:24:40 +0200

    api root added

--- finance/urls.py
+++ finance/urls.py
@@ -4,12 +4,13 @@ from finance import views
 from django.conf.urls import include
 
 urlpatterns = [
-    path('investmentuser/', views.InvestmentUserList.as_view()),
+    path('investmentuser/', views.InvestmentUserList.as_view(), name="user-list"),
     path('investmentuser/<int:pk>/', views.InvestmentUsertDetail.as_view()),
-    path('portfoliomanagement/', views.PortfolioManagementList.as_view()),
+    path('portfoliomanagement/', views.PortfolioManagementList.as_view(), name="portfolio-management-log"),
     path('portfoliomanagement/<int:pk>/', views.PortfolioManagementDetail.as_view()),
-    path('stock/', views.StockList.as_view()),
+    path('stock/', views.StockList.as_view(), name="stock-list"),
     path('stock/<int:pk>/', views.StockDetail.as_view()),
+    path('', views.api_root),
 ]
 urlpatterns += [
     path('api-auth/', include('rest_framework.urls')),
--- finance/views.py
+++ finance/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
 from rest_framework import permissions
-
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from rest_framework import generics
-
+from rest_framework.decorators import api_view
 from finance.models import InvestmentUser, PortfolioManagement, Stock
+from finance.permissions import IsOwnerOrReadOnly
 from finance.serializers import InvestmentUserSerializer, PortfolioManagementSerializer, StockSerializer
 
 
@@ -39,4 +40,13 @@ class PortfolioManagementList(generics.ListCreateAPIView):
 class PortfolioManagementDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = PortfolioManagement.objects.all()
     serializer_class = PortfolioManagementSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'portfolio management': reverse('portfolio-management-log', request=request, format=format),
+        'stocks': reverse('stock-list', request=request, format=format)
+    })
